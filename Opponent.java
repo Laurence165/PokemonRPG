a6feddbf8e626e2270e1a89ac267c241d98eafd2
liulaur3 <laurenceliu.liu@mail.utoronto.ca>
2023-11-21 14:44:14 -0500

    NPC, NPCTests,Opponent, Villager

---
+++ Opponent.java
@@ -0,0 +1,17 @@
+public class Opponent extends NPC{
+
+
+    public Opponent(String name, String description, String image_address, String audio_address, String[] phrases) {
+        super(name, description, image_address, audio_address, phrases);
+    }
+
+    public Array<Pokemon> select_Pokemon(){
+
+    }
+    public Move attack(Pokemon active){
+
+    }
+
+
+
+}
